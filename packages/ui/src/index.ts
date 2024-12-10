b7c9d75c9631d1edf038e0aa94cb819a7b2971f9
Ahamad Kutagolla <ahamad_kutagolla@partech.com>
2024-12-10 22:45:40 +0530

    feat(ui): add PersonalizedBanner component for account selection and greeting

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,4 +2,4 @@ export { Button } from './button';
 export { Card } from './card';
 export { Code } from './code';
 export { StickerSheet } from './StickerSheet';
-export { PersonalizedBanner } from './Banner';
+export { PersonalizedBanner } from './PersonalizedBanner';
